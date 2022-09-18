df68eb541d5633f76ae7e61b510333a5810de074
Ta Phuc <hoangphucnb97@gmail.com>
2022-09-18 21:58:31 +0700

    Add contract StakingContract

---
+++ contracts/StakingContractUpgradeable.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+contract StakingContractUpgradeable is Initializable, OwnableUpgradeable {
+    using SafeERC20Upgradeable for IERC20PermitUpgradeable;
+
+    struct PhaseInfo {
+        uint256 startTime;
+        uint256 endTime;
+        uint256 totalReward;
+        uint256 totalDistribution;
+    }
+
+    PhaseInfo[] public phases;
+
+    //Distribution of user for each phase
+    mapping(address => uint256) userContributionInPhase;
+
+    //Reward user got for each phase, non-zero mean user got reward
+    mapping(address => uint256) rewardUserGotInPhase;
+
+    uint256 public currentPhase;
+
+    IERC20Upgradeable stakeToken;
+    IERC20Upgradeable rewardToken;
+
+    event PhaseStarted(uint256 indexed currentPhase);
+
+    function initialize(address stakeToken_, address rewardToken_) public initializer {
+        stakeToken = IERC20Upgradeable(stakeToken_);
+        rewardToken = IERC20Upgradeable(rewardToken_);
+    }
+
+    function startPhase() external onlyOwner {
+        require(currentPhase == 0 || phases[currentPhase - 1].endTime != 0, "startPhase::Previous phase not ended");
+        PhaseInfo storage currentPhaseInfo = phases[currentPhase];
+        currentPhaseInfo.startTime = block.timestamp;
+
+        emit PhaseStarted(currentPhase);
+    }
+}
